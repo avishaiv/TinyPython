9e716142e14a057e1af7e131444e82a21ff38864
avishaiv <avishaiv@gmail.com>
2023-03-26 17:38:36 +0300

    added comment 1

--- example.py
+++ example.py
@@ -14,7 +14,7 @@ def anotherFunction(userInput):
     command = 'cat "{userInput}"'.format(source=userInput)
     subprocess.call(command, shell=True)
 
-
+# comment 1g
 #def thirdFunction(userInput):
 #    req = Request(userInput)
 #    resp = req.Response()
